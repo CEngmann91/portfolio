62f3c5d12d51a2392e05dfc3be2363ce9734b735
CEngmann91 <christian.j.engmann@googlemail.com>
2022-09-13 14:32:54 +0100

    react-router-hash-link, react-router-dom, functioning navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
-import './App.css';
 import Navbar from './components/navbar/Navbar';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { useThemeContext } from './utils/ThemeContext';
+import { Landing, About, Projects, Skills, Footer } from './pages';
 
 function App() {
   const { theme } = useThemeContext();
 
+
+  const renderRoutes = (
+    <>
+      <Route exact path="/" element={<Landing />} />
+      <Route exact path="/about" element={<About />} />
+      <Route exact path="/projects" element={<Projects />} />
+      <Route exact path="/skills" element={<Skills />} />
+      <Route exact path="/footer" element={<Footer />} />
+      {/* <Route path="/404" component={() => <h1>Not Found!</h1>} /> */}
+      {/* <Redirect to="/404" /> */}
+    </>
+  )
+
   return (
-    <div id={theme} className="App">
+    <Router id={theme}>
       <Navbar />
-    </div>
+
+      <Routes>
+        {/* { renderRoutes() } */}
+
+        <Route exact path="/" element={<Landing />} />
+        <Route exact path="/about" element={<About />} />
+        <Route exact path="/projects" element={<Projects />} />
+        <Route exact path="/skills" element={<Skills />} />
+        <Route exact path="/footer" element={<Footer />} />
+        {/* <Route path="/404" component={() => <h1>Not Found!</h1>} /> */}
+        {/* <Redirect to="/404" /> */}
+
+      </Routes>
+    </Router>
   );
 }
 
