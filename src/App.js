1771212d5749d42e8d2a8c7f5d82edc49309f13c
CEngmann91 <christian.j.engmann@googlemail.com>
2022-09-13 15:36:33 +0100

    test build 4

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import Navbar from './components/navbar/Navbar';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // import { useThemeContext } from './utils/ThemeContext';
-// import { Landing, About, Projects, Skills, Footer } from './pages';
 import Landing from './pages/Landing/Landing';
 import About from './pages/About/About';
 import Projects from './pages/Projects/Projects';
