a8743fec75c0dbe338e7e818670d0121acdc74ad
CEngmann91 <christian.j.engmann@googlemail.com>
2022-09-13 16:51:59 +0100

    updated navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './Navbar.css';
 import { NavLink } from 'react-router-dom';
 import { APP_NAME, NAVBAR } from '../../utils/constants';
@@ -6,29 +6,39 @@ import { useThemeContext } from '../../utils/ThemeContext';
 
 const Navbar = () => {
     const { theme, toggleTheme } = useThemeContext();
+    const [scrolledDown, setScrolledDown] = useState(false);
+    const [resumeVisible, setResumeVisible] = useState(false);
     const [menuIsOpen, setMenuIsOpen] = useState(false)
 
 
+    useEffect(() => {
+        setTimeout(() => setResumeVisible(true), 1000)
+        window.addEventListener('scroll', onScroll)
+        return () => window.removeEventListener('scroll', onScroll)
+    }, [])
+
+    const onScroll = () => {
+        setScrolledDown((window.scrollY > 100))
+    }
+
     return (
-        <nav id={theme} className='navbar-nav'>
-            <div className='navbar-center'>
-                <div className='navbar-wrapper'>
-                    <NavLink className='ce-title-circle' to="/">
-                        <label className='title'>CE</label>
-                    </NavLink>
-                </div>
-
-                <ul className={menuIsOpen ? "navbar-links show-nav" : "navbar-links"}>
-                    {NAVBAR.MENU_DATA.map(({ title, to }, index) =>
-                        <li>
-                            <NavLink to={to} key={index}>{title}</NavLink>
-                        </li>
-                    )}
-                </ul>
-
+        <nav id={theme} className={`navbar-nav ${scrolledDown ? 'navbar-nav--scroll' : ''}`}>
+        {/* <nav id={theme} className='navbar-nav'> */}
+            <div className='navbar-wrapper'>
+                <NavLink className='ce-title-circle' to="/">
+                    <label className='title'>CE</label>
+                </NavLink>
             </div>
 
-            <button className='navbar-temp-mode-toggle' onClick={toggleTheme}>Change to {theme} mode</button>
+            <ul className={menuIsOpen ? "navbar-links show-nav" : "navbar-links"}>
+                {NAVBAR.MENU_DATA.map(({ title, to }, index) =>
+                    <li>
+                        <NavLink to={to} key={index}>{title}</NavLink>
+                    </li>
+                )}
+            </ul>
+
+            <button className='navbar-temp-mode-toggle' onClick={toggleTheme}>{theme}</button>
         </nav>
     )
 }
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,11 +4,11 @@ export const MODE = 'dark';
 export const NAVBAR = {
 
     MENU_DATA: [
-        { title: 'Home',               to: '/' },
-        { title: 'About',              to: 'about' },
-        { title: 'Projects',           to: 'projects' },
-        { title: 'Skills',             to: 'skills' },
-        // { title: 'View My C.V.',         to: '',             selectableClassName: false,       url: CV_URL },
-        // { title: 'Request A Project >',  to: 'hire',         selectableClassName: false },
+        { title: 'Home',                 to: '/' },
+        { title: 'About',                to: 'about' },
+        { title: 'Projects',             to: 'projects' },
+        { title: 'Skills',               to: 'skills' },
+        { title: 'View My C.V.',         to: 'cv' },
+        { title: 'Request A Project >',  to: 'hire' },
     ]
 }
