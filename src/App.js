86287c87a20fc6b7c0bdc4aa5e0eed924e8a3bb8
CEngmann91 <christian.j.engmann@googlemail.com>
2022-09-13 15:48:17 +0100

    test build 6

--- src/App.js
+++ src/App.js
@@ -1,46 +1,33 @@
-import Navbar from './components/navbar/Navbar';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import { useThemeContext } from './utils/ThemeContext';
+import { useThemeContext } from './utils/ThemeContext';
+import Navbar from './components/navbar/Navbar';
 import Landing from './pages/Landing/Landing';
 import About from './pages/About/About';
 import Projects from './pages/Projects/Projects';
 import Skills from './pages/Skills/Skills';
 import Footer from './pages/Footer/Footer';
+import NotFound from './pages/NotFound/NotFound';
+
 
 function App() {
-  // const { theme } = useThemeContext();
-
-
-  const renderRoutes = (
-    <>
-      <Route exact path="/" element={<Landing />} />
-      <Route exact path="/about" element={<About />} />
-      <Route exact path="/projects" element={<Projects />} />
-      <Route exact path="/skills" element={<Skills />} />
-      <Route exact path="/footer" element={<Footer />} />
-      {/* <Route path="/404" component={() => <h1>Not Found!</h1>} /> */}
-      {/* <Redirect to="/404" /> */}
-    </>
-  )
+  const { theme } = useThemeContext();
 
   return (
-    <Router
-    // id={theme}
-    >
+    <Router id={theme}>
       <Navbar />
 
       <Routes>
-        {/* { renderRoutes() } */}
+
+        {/* Use it in this way, and it should work: */}
+        <Route path='*' element={<NotFound />} />
 
         <Route exact path="/" element={<Landing />} />
         <Route exact path="/about" element={<About />} />
         <Route exact path="/projects" element={<Projects />} />
         <Route exact path="/skills" element={<Skills />} />
         <Route exact path="/footer" element={<Footer />} />
-        {/* <Route path="/404" component={() => <h1>Not Found!</h1>} /> */}
-        {/* <Redirect to="/404" /> */}
-
+        
       </Routes>
     </Router>
   );
--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-// import { ThemeProvider } from './utils/ThemeContext';
+import { ThemeProvider } from './utils/ThemeContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <ThemeProvider> */}
+    <ThemeProvider>
       <App />
-    {/* </ThemeProvider> */}
+    </ThemeProvider>
   </React.StrictMode>
 );
