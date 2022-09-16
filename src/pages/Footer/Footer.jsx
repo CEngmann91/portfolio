25fd9e1c41beefab5b3b1835d3eacba12d4e273c
CEngmann91 <christian.j.engmann@googlemail.com>
2022-09-16 20:06:52 +0100

    footer updated

--- src/pages/Footer/Footer.jsx
+++ src/pages/Footer/Footer.jsx
@@ -1,10 +1,55 @@
 import React from 'react'
 import './Footer.scss';
+import { Twitter, Facebook, Linkedin } from '../../utils/icons';
 
 const Footer = () => {
   return (
     <div className='app__footer'>
-        Footer
+      {/* Footer */}
+
+
+      <div className='app__footer--contents'>
+
+        <div className='socials'>
+          <p className='title'>Socials</p>
+          {/* <p>Insert Text here</p> */}
+          <div className='links'>
+            <div className='links-icon'><Facebook /></div>
+            <div className='links-icon'><Twitter /></div>
+            <div className='links-icon'><Linkedin /></div>
+          </div>
+        </div>
+
+
+
+        <div className='categories'>
+          <p className='title'>Categories</p>
+          <p>Insert Text here</p>
+        </div>
+
+        <div className='quick-links'>
+          <p className='title'>Quick Links</p>
+          <p>Insert Text here</p>
+        </div>
+      </div>
+
+      <div className='app__footer--baseline'>
+        <div className='line' />
+
+        <div className='container'>
+          <p className='copyright'>Copyright © 2022 All Rights Are Reserved by Christian Engmann</p>
+
+          {/* <div className='links'>
+            <div className='links-icon'><Facebook /></div>
+            <div className='links-icon'><Twitter /></div>
+            <div className='links-icon'><Linkedin /></div>
+          </div> */}
+
+        </div>
+      </div>
+
+
+
     </div>
   )
 }
---
+++ src/utils/icons.js
@@ -0,0 +1,8 @@
+import { BsTwitter } from 'react-icons/bs';
+import { FaFacebookF, FaLinkedinIn } from 'react-icons/fa';
+
+export {
+    BsTwitter as Twitter,
+    FaFacebookF as Facebook,
+    FaLinkedinIn as Linkedin,
+};
