446076a6620cc42e794e27ff001d1af552277189
CEngmann91 <christian.j.engmann@googlemail.com>
2022-11-01 06:58:56 +0000

    revamped website. Added messages, projects with filters and more

---
+++ src/components/HeartFeltMessage/HeartFeltMessage.tsx
@@ -0,0 +1,29 @@
+import './HeartFeltMessage.scss';
+import React from 'react'
+import AnimatedMessage from '../AnimatedMessage/AnimatedMessage';
+
+const message = {
+    content: [
+        <h2 className="head-text">{'<'}Do What <span>You Love,</span></h2>,
+        <h2 className="head-text">Love What <span>You Do{`/>`}</span></h2>
+    ],
+    variants: {
+        visible: { opacity: 1, y: 0 },
+        hidden: { opacity: 0, y: 100 }
+    },
+    duration: 1,
+    delay: 0
+}
+const HeartFeltMessage = () => {
+    return (
+        <div className='app__flex main'>
+            {message.content.map((paragraph, index) => (
+                <AnimatedMessage delay={(index * 1.5)}>
+                    {paragraph}
+                </AnimatedMessage>
+            ))}
+        </div>
+    )
+}
+
+export default HeartFeltMessage
---
+++ src/pages/Projects/ProjectCard/ProjectCard.tsx
@@ -0,0 +1,83 @@
+import './ProjectCard.scss';
+import React from 'react'
+import { motion } from 'framer-motion'
+import { iProject, Link, Tags } from '../Projects';
+import { Codepen, Github, ReactIcon, FirebaseIcon, UnityIcon, MobileIcon, YoutubeIcon } from '../../../utils/icons';
+
+const variantions = {
+    variants: {
+        hidden: { opacity: 0, x: 200 },
+        visible: { opacity: [0, 0.2, 0.3, 1], x: 0 },
+    },
+    ease: "easeIn",
+    duration: 0.3,
+    delay: 0.4,
+}
+
+const ProjectCard: React.FC<iProject> = (item, { ...props }: iProject) => {
+    return (
+        <motion.div
+            key={item.id}
+            className="card"
+            variants={variantions.variants}
+            initial="hidden"
+            whileInView="visible"
+            style={item.gradient ? { background: `linear-gradient(${item.gradient[0]}, ${item.gradient[1]});` } : {}}
+            // Play in viewport only once.
+            viewport={{ once: true }}
+            transition={{
+                ease: variantions.ease,
+                duration: variantions.duration,
+                delay: ((item.id % 3) * variantions.delay),
+            }}
+        >
+
+            <div className='card-title'>
+                {/* <h3>{item.title} {item.gradient[0]} {item.gradient[1]}</h3> */}
+                <h3>{item.title}</h3>
+            </div>
+
+            <div className='card-img'>
+                <img src={item.imgUrl} alt={item.title} />
+                {/* <img src={urlFor(item.imgUrl)} alt={item.title} /> */}
+            </div>
+
+            {/* <div className='card-description'>
+                <p>{item.description}</p>
+              </div> */}
+
+            <div className='card-footer'>
+                {/* Techs used for project. */}
+                <div className='card-footer-techs'>
+                    {item.tags?.map((item) => {
+                        switch (item) {
+                            case Tags.React: return (<ReactIcon />)
+                            case Tags.ReactNative: return (<ReactIcon />)
+                            case Tags.Firebase: return (<FirebaseIcon />)
+                            case Tags.Unity: return (<UnityIcon />)
+                            case Tags.MobileApp: return (<MobileIcon />)
+                            case Tags.YouTube: return (<YoutubeIcon />)
+                            default: break;
+                        }
+                    })}
+                </div>
+
+                {/* Links for the project/website. */}
+                <div className='card-footer-links'>
+                    {item.links?.map((item) => {
+                        switch (item.link) {
+                            case Link.Codepen:
+                                return (<a href={item.url} className='card-links-item app__hover-with-shadow'><Codepen /></a>)
+                            case Link.Github:
+                                return (<a href={item.url} className='card-links-item app__hover-with-shadow'><Github /></a>)
+                            case Link.YouTube: return (<a href={item.url} className='card-links-item app__hover-with-shadow'><YoutubeIcon /></a>)
+                            default: break;
+                        }
+                    })}
+                </div>
+            </div>
+        </motion.div>
+    )
+}
+
+export default ProjectCard
